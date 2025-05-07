b08c9c45550cef293cd1fb1680bca18bcc5025e6
Vilhelm-WG <haisiuk.vilhelm@chnu.edu.ua>
2025-05-07 12:04:54 +0300

    add struct Book

---
+++ book-list.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct Book {
+    std::string title;
+    std::string author;
+    int year;
+    std::string genre;
+    bool isRead;
+};
