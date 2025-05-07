1dd28a577d7e3748912e4f499a175ab22cd3dfaa
Vilhelm-WG <haisiuk.vilhelm@chnu.edu.ua>
2025-05-07 12:06:28 +0300

    add void addBokk

--- book-list.cpp
+++ book-list.cpp
@@ -9,3 +9,23 @@ struct Book {
     std::string genre;
     bool isRead;
 };
+
+void addBook(std::vector<Book>& library) {
+    Book newBook;
+    std::cout << "Назва книги: ";
+    std::getline(std::cin, newBook.title);
+    std::cout << "Автор: ";
+    std::getline(std::cin, newBook.author);
+    std::cout << "Рік видання: ";
+    std::cin >> newBook.year;
+    std::cin.ignore(); // очищення буфера
+    std::cout << "Жанр: ";
+    std::getline(std::cin, newBook.genre);
+    char readInput;
+    std::cout << "Прочитана (y/n): ";
+    std::cin >> readInput;
+    newBook.isRead = (readInput == 'y' || readInput == 'Y');
+    std::cin.ignore();
+    library.push_back(newBook);
+    std::cout << "Книгу додано!\n";
+}
